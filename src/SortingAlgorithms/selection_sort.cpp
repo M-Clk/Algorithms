92eea37e35a26cc203fad62dca451a2452817bf1
Muhammed CELIK <muhammed.celik@adayazilim.com>
2022-09-07 23:56:06 +0300

    Add selection sort algorithm.

---
+++ src/SortingAlgorithms/selection_sort.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "../common/common.cpp"
+using namespace std;
+
+void selectionSort()
+{
+    cout << "SORTING ALGORITHMS #Selection Sort\n";
+    int array[] = {5, 34, 4, 1, 7, 34, 67, -32, 76, 2, 3, 5};
+    int arraySize = sizeof(array)/sizeof(array[0]);
+    cout<<"Unsorted Array: ";
+    printArray(array, arraySize);
+    for (int i = 0; i < arraySize; i++)
+    {
+        for (int j = i + 1; j < arraySize; j++)
+        {
+            if(array[i] <= array[j])
+            continue;
+            
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+            
+        }
+    }
+
+    cout<<"\nSorted Array: ";
+    printArray(array, arraySize);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include "SortingAlgorithms/selection_sort.cpp"
 
 using namespace std;
 
 int main()
 {
     cout<<"ALGORITHMS"<<endl;
+    selectionSort();
     return 0;
 }
